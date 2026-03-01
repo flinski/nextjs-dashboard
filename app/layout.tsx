d9a2af877947c4e777ad01ab51a04899707e09bc
Vladislav Flinski <flinssski@gmail.com>
2026-03-01 15:25:04 +0300

    chore: add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import '@/app/ui/global.css'
+
+interface RootLayoutProps {
+	children: React.ReactNode
 }
+
+function RootLayout({ children }: RootLayoutProps) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	)
+}
+
+export default RootLayout
